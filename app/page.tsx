0a7dca31057191fef370ad5e79e4abe22449c786
cbabi2023 <info@abijithcb.com>
2025-12-23 23:14:20 +0530

    Update to Next.js 16.1.1 and prepare for Vercel deployment

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,6 @@ import HeroSection from "@/components/HeroSection";
 import ThoughtOfTheDay from "@/components/ThoughtOfTheDay";
 import KnowledgePillars from "@/components/KnowledgePillars";
 import ContinueReading from "@/components/ContinueReading";
-import OfflineLibrary from "@/components/OfflineLibrary";
-import FeaturedWork from "@/components/FeaturedWork";
-import GuruAndSociety from "@/components/GuruAndSociety";
-import VisualArchive from "@/components/VisualArchive";
-import ResearchArticles from "@/components/ResearchArticles";
 import Footer from "@/components/Footer";
 import MobileNav from "@/components/MobileNav";
 
@@ -19,11 +14,6 @@ export default function Home() {
       <ThoughtOfTheDay />
       <KnowledgePillars />
       <ContinueReading />
-      <OfflineLibrary />
-      <FeaturedWork />
-      <GuruAndSociety />
-      <VisualArchive />
-      <ResearchArticles />
       <Footer />
       <MobileNav />
     </main>
